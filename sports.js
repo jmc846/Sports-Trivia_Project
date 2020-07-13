12bf182554c7b99e8edd4cf59501c20c297f7480
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-07-13 18:58:05 -0400

    created js psuedo code

---
+++ sports.js
@@ -0,0 +1,16 @@
+/* psuedo code for quiz
+the user is presented with prompt for name/email(modal)
+the user is presented with a start button(use modal here)
+the user clicks the start button (use on click function here)
+a new page appears with instructions (modal(header)) /Additional button (on click)
+questions appear(20+) (each show seconds elasped plus actual time) moment js shown here-pulled from espn api
+if question answer incorrect deduction in time pops next question (if conditional --)(json to missed)
+if question correct, modal messages, (json to score )
+quiz continues until either timer elapses or all question answered
+
+user presented with score /missed data and time elaspsed. --
+
+
+
+
+*/
