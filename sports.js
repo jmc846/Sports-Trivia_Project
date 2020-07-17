5d79540a0decbf887599ba96679ec9b395fdc1be
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-07-16 20:20:18 -0400

    javascript functions variable adds

--- sports.js
+++ sports.js
@@ -1,16 +1,173 @@
-/* psuedo code for quiz
-the user is presented with prompt for name/email(modal)
-the user is presented with a start button(use modal here)
-the user clicks the start button (use on click function here)
-a new page appears with instructions (modal(header)) /Additional button (on click)
-questions appear(20+) (each show seconds elasped plus actual time) moment js shown here-pulled from espn api
-if question answer incorrect deduction in time pops next question (if conditional --)(json to missed)
-if question correct, modal messages, (json to score )
-quiz continues until either timer elapses or all question answered
+//* variables that are global on top */
+var questionsObj = [{
+	"question": "what is the capital of india?",
+	"options": ["hyderabad", "delhi", "chennai", "bengalore"],
+	"answer": "hyderabad"
+}, {
+	"question": "what is even number?",
+	"options": [1, 3, 5, 8],
+	"answer": 8
+}];
+// var answerChoices = [{}];
+// var answerSelected = document.querySelector$(onclick)
+var rightAnswers = document.querySelector("rightAnswers");
+// var currentTime = $("<div>").append(currentTime)
+// // var score = {};
 
-user presented with score /missed data and time elaspsed. --
+// API info NYT
+var queryUrl = ("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=76F4by9ktUvjFKC7kIl7vqi7REdEE6JA");
+ApiKey = "76F4by9ktUvjFKC7kIl7vqi7REdEE6JA";
+AppId = "e12869a3-d720-4808-8150-476679bf0862";
+
+// API info SportsRadar
+// MLB Trial
+// Sports Trivia and Statistics
+// Key: a46agsaangqu3rr7st7fze3j
+var queryUrl1 = ("http://api.sportradar.us/mlb/trial/v6.6/en/seasons/2019/REG/teams/aa34e0ed-f342-4ec6-b774-c79b47b60e2d/statistics.json?api_key=a46agsaangqu3rr7st7fze3j");
+
+// NBA Trial
+// Sports Trivia and Statistics
+// Key: nqupruujs87vehqsjscfkv38
+var queryUrl2 = ("http://api.sportradar.us/nba/trial/v7/en/seasons/2018/REG/teams/583eca2f-fb46-11e1-82cb-f4ce4684ea4c/statistics.json?api_key=nqupruujs87vehqsjscfkv38");
+
+// NCAA Football Trial v1
+// Sports Trivia and Statistics
+// Key: ssp447yux4j5bqzzefsma78b
+var queryUrl3 = ("http://api.sportradar.us/ncaafb-t1/teams/BAMA/2018/REG/statistics.json?api_key=ssp447yux4j5bqzzefsma78b");
+
+// NCAAMB Trial
+// Sports Trivia and Statistics
+// Key: qgzqf5ped5dfn4kzjggwgdvw
+var queryUrl4 = ("http://api.sportradar.us/ncaamb/trial/v7/en/seasons/2019/REG/teams/c10544de-e3bd-4776-ba2e-83df8c017fd1/statistics.json?api_key=9xaupn4c7mnn3xapz6p7kcbn");
+
+// NFL Official Trial
+// Sports Trivia and Statistics
+// Key: wpnsv5hxury3ta6nkrskyd6k
+var queryUrl5 = ("http://api.sportradar.us/nfl-ot1/seasontd/2018/REG/teams/97354895-8c77-4fd4-a860-32e62ea7382a/statistics.json?api_key=wpnsv5hxury3ta6nkrskyd6k");
+
+/* logic functions */
+// questions object appears as page loads
+$(document).ready(displayQuiz);
+
+
+var compareAnswers = {};
+
+
+
+/* call back functions*/
+// Here we loop through our array using the .each() method and append a new div with each iteration/questions
+function displayQuiz = $("li").each(questionsObj, function(i, rollQuestions) {
+	questionDiv.append("<p>" + rollQuestions + "</p>");
+});
+
+// function() { };
+displayResults = function() {};
+
+function(scrollStats) {
+	 = $(".nbaStat").on("click", showStats);
+	   $("li").append(json.stringify)
+
+
+
+	function rollQuestions() {
+		// console.log(questions);start question display
+	}
+	// console.log(event);
+	$("li").append(questionsObj[i]);
+	for (var i = 0; i < questionsObj.length; i++) {
+		console.log(questionsObj[i].q);
+
+		$('li').append(questionsObj[i].q);
+	}
+	/* event handling functions , all your button clicking*/
+	// event to display questions, scroll stats, display results
+	$(document).ready()
+
+		console.log("works");
+
+
+		$("#nbaStat", "#nflStat", "#mlbStat", "#nhlStat").append("click", function() {
+			console.log("I see you");
+
+			//naming the website which we should use in the ajax
+			queryUrl = ("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=76F4by9ktUvjFKC7kIl7vqi7REdEE6JA");
+			//Pulling the random stats from these websites
+			function showStats() {
+				$.ajax({
+
+						url: queryURL,
+						queryUrl1,
+						queryUrl2,
+						queryUrl3,
+						queryUrl4,
+						queryUrl5,
+						method: "GET"
+					})
+					// Response for the ajax function
+					.then(function(response) {
+						$(".menu").text(JSON.stringify(response));
+
+					});
+			};
 
 
 
 
-*/
+
+
+
+
+
+		});
+
+		/* psuedo code for quiz
+		the user is presented with prompt for name/email(modal)
+
+		the user is presented with a start button(use modal here)
+
+		the user clicks the start button (use on click function here)
+		$("start").on("click",  function (event) {
+		  event.preventDefault();
+
+
+		  function rollQuestions() {
+		    // console.log(questions);start question display
+		};
+		  // console.log(event);
+		  $("questions-container").append(questions.i)
+		  for (var i = 0; i <questions.length; i++) {}
+
+
+
+		a new page appears with instructions (modal(header)) /Additional button (on click)
+		questions appear(20+) (each show seconds elasped plus actual time) moment js shown here-pulled from espn api
+		if question answer incorrect deduction in time pops next question (if conditional --)(json to missed)
+		if question correct, modal messages, (json to score )
+		quiz continues until either timer elapses or all question answered
+
+		user presented with score /missed data and scroll stats  --*/
+
+		function saveTriviaData() {
+			$.ajax({
+				url: queryURL,
+				queryUrl1,
+				queryUrl2,
+				queryUrl3,
+				queryUrl4,
+				queryUrl5
+			});
+			method: "GET"
+				.then(function(response) {
+					console.log("works")
+					$(".menu").append(JSON.stringify(response));
+
+				});
+		}
+	});
+
+	// rollQuestions
+
+
+
+	// }}
+}
